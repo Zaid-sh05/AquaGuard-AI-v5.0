922ad542783a56c24f3275cf9af25e7d81a55663
Zaid Alshobaki <192496198+Zaid-sh05@users.noreply.github.com>
2026-05-29 17:55:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AquaGuard-AIv5.0/',
-});
+  base: '/AquaGuard-AI-v5.0/',
+})
